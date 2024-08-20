bdaf1607d4a3a6188370f33aa03ed47739b7f06f
iidaaald <iida.aaldijk@metropolia.fi>
2024-08-20 15:37:27 +0300

    valmis

---
+++ moduuli 2/tehtävä2.4.py
@@ -0,0 +1,11 @@
+luku1 = float(input("anna ensimmäinen kokonaisluku:"))
+luku2 = float(input("anna toinen kokonaisluku:"))
+luku3 = float(input("anna kolmas kokonaisluku:"))
+
+summa = luku1 + luku2 + luku3
+tulo = luku1*luku2*luku3
+keskiarvo = (luku1 + luku2 + luku3)/3
+
+print("lukujen summa:" + str(summa))
+print("lukujen tulo:" + str(tulo))
+print("lukujen keskiarvo:" + str(keskiarvo))
