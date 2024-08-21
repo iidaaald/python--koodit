0d3c3aba23c6d8870a33c1dbc74238879bc89d06
iidaaald <iida.aaldijk@metropolia.fi>
2024-08-21 08:07:43 +0300

    valmis

--- moduuli 2/tehtävä2.3.py
+++ moduuli 2/tehtävä2.3.py
@@ -1,8 +1,8 @@
 kanta_str = input("anna kanta:")
-korkeus_str = input("anna korkeus")
+korkeus_str = input("anna korkeus:")
 kanta = float(kanta_str)
 korkeus = float(korkeus_str)
 pintala = kanta*korkeus
 piiri = (2*kanta)+(2*korkeus)
-print("suorakulmion pinta-ala" + str(pintala))
-print("suorakulmion piiri-ala" + str(piiri))
+print("suorakulmion pinta-ala:" + str(pintala))
+print("suorakulmion piiri-ala:" + str(piiri))
