112e6dcbedb99985c74e8e4d51e26eaaee677c2a
iidaaald <iida.aaldijk@metropolia.fi>
2024-08-21 14:09:04 +0300

    valmis

---
+++ moduuli 2/tehtävä2.5.py
@@ -0,0 +1,12 @@
+leiviskä = float(input("Anna leiviskät:"))
+naula = float(input("Anna naulat:"))
+luoti = float(input("Anna naulat:"))
+
+luotipaino = 13.3*luoti
+naulapaino = naula*(32*13.3)
+leiviskäpaino = leiviskä*(20*(32*13.3))
+
+kilot = float((luotipaino + naulapaino + leiviskäpaino)//1000)
+grammat = float((luotipaino + naulapaino + leiviskäpaino) % 1000)
+
+print("Massa nykymittojen mukaan:" '\n' + str(kilot) + "kg ja " + str(grammat) + "g." )
