c7673de152cff3649aee76135b26774cad3af110
iidaaald <iida.aaldijk@metropolia.fi>
2024-08-22 15:04:21 +0300

    valmis

--- moduuli 2/tehtävä2.5.py
+++ moduuli 2/tehtävä2.5.py
@@ -6,7 +6,10 @@ luotipaino = 13.3*luoti
 naulapaino = naula*(32*13.3)
 leiviskäpaino = leiviskä*(20*(32*13.3))
 
-kilot = float((luotipaino + naulapaino + leiviskäpaino)//1000)
-grammat = float((luotipaino + naulapaino + leiviskäpaino) % 1000)
+kilot = (luotipaino + naulapaino + leiviskäpaino)//1000
+grammat = (luotipaino + naulapaino + leiviskäpaino) % 1000
 
 print("Massa nykymittojen mukaan:" '\n' + str(kilot) + "kg ja " + str(grammat) + "g." )
+
+
+
---
+++ moduuli 2/tehtävä2.6.py
@@ -0,0 +1,12 @@
+import random
+
+eka =  (random.randint(0,9))
+toka = (random.randint(0,9))
+kolmas =  (random.randint(0,9))
+neljäs = (random.randint(1,6))
+viides =  (random.randint(1,6))
+kuudes = (random.randint(1,6))
+seit =  (random.randint(1,6))
+
+print("kolmenumeroinen koodi: " + str(eka) + str(toka) + str(kolmas))
+print("nelinumeroinen koodi: " + str(neljäs) + str(viides) + str(kuudes) + str(seit))
